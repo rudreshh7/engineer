db69252176c5eb4759a85e47a1e71ab2d3a99cc2
Rudresh Jha <kumarrudresh5@gmail.com>
2024-06-20 21:16:28 +0530

    just take it

--- src/components/WorkCard.tsx
+++ src/components/WorkCard.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 import Image from "next/image";
 
-const WorkCard = ({ id, name, workImg, icon, description }) => {
+interface WorkCardProps {
+  id: number;
+  name: string;
+  workImg: string;
+  icon: string;
+  description: string;
+}
+
+const WorkCard: React.FC<WorkCardProps> = ({
+  id,
+  name,
+  workImg,
+  icon,
+  description,
+}) => {
   return (
     <div>
       <Image
@@ -9,10 +23,10 @@ const WorkCard = ({ id, name, workImg, icon, description }) => {
         width={1920}
         height={1080}
         className="max-w-full"
-        alt="work images"
+        alt={name}
       />
       <div className="flex items-center gap-2">
-        <Image src={icon} width={20} height={20} alt="icons" />
+        <Image src={icon} width={20} height={20} alt={`${name}icon`} />
         <p className="font-bold">{name}</p>
       </div>
       <p className="text-xs">{description}</p>
